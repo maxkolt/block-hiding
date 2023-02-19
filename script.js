33febd061ba9cbc25ac6bb248bde9e80556fbf0f
maximkoltovich <12345kolt@gmail.com>
2023-02-19 19:14:06 +0300

    Написал фкнцию blockHiding

--- script.js
+++ script.js
@@ -8,22 +8,20 @@ function removeBanner() {
 const lastViewKey = 'lastView';
 
 function blockHiding() {
-    localStorage.setItem(lastViewKey, new Date().toString())
-    const key = 'numberOfViews';
-    const numberOfViews = localStorage.getItem(key)
-    if (!numberOfViews) {
-      localStorage.setItem(key, 1)
-      return
-    }
-    const newNumberOfViews = Number(numberOfViews) + 1
-    localStorage.setItem(key, newNumberOfViews)
+  localStorage.setItem(lastViewKey, new Date().toString())
+  const key = 'numberOfViews';
+  const numberOfViews = localStorage.getItem(key)
+  if (!numberOfViews) {
+    localStorage.setItem(key, 1)
+    return
+  }
+  const newNumberOfViews = Number(numberOfViews) + 1
+  localStorage.setItem(key, newNumberOfViews)
 
-    if (newNumberOfViews <= 3) {
-      return;
-    }
-    removeBanner()
+  if (newNumberOfViews <= 3) {
+    return;
+  }
+  removeBanner()
 }
-
-// removeBanner()
 
 blockHiding()
